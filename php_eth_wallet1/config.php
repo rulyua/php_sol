78b5cfba85424fc57718bad3167bf02a189d5475
Ruslan <rulyua@gmail.com>
2025-11-17 15:58:12 +0200

    test5.

--- php_eth_wallet1/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-return [
-    "rpc" => "http://127.0.0.1:8545",
-    "private_key" => "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
-    "contract_address" => "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-];
--- php_eth_wallet1/public/api/get_balance.php
+++
@@ -1,6 +0,0 @@
-<?php
-$config = require "../../config.php";
-require "../../src/Wallet.php";
-
-$w = new Wallet($config);
-echo json_encode(["balance" => $w->getContractBalance()]);
